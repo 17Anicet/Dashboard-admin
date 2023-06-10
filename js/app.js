3ec66b4f81280d95eb8774f53373bdaa6b69c506
unknown <chizaanicet@gmail.com>
2023-06-10 16:50:29 +0200

    add to my repository

--- js/app.js
+++ js/app.js
@@ -17,10 +17,12 @@ document.addEventListener('DOMContentLoaded',  function(){
         },
         series: [{
             name:'Synopis',
-            data:[2.0,0.5,0.3,1.5,0.3,0.6,1.0]
+            data:[2.0,0.5,0.3,1.5,0.3,0.6,1.0],
+            color: '#000620'
         },{
             name:'Total time',
-            data: [0.5,1.5,1.0,0.4,1.0,2.0,0.3]
+            data: [0.5,1.5,1.0,0.4,1.0,2.0,0.3],
+            color: '#58585a'
         }]
     });
 });
--- js/colors.js
+++ js/colors.js
@@ -9,7 +9,6 @@ document.addEventListener('DOMContentLoaded',function(){
         },
         colors:['#578756','#286886','#0000FF','#FFFF00'],
         series: [{
-            
                 name:'Data',
                 data:[5,10,15,20]
         }]
