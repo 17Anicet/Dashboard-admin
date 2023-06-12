ad58c77700c404a066dfc731c8154baa255cfdb2
unknown <chizaanicet@gmail.com>
2023-06-12 21:29:42 +0200

    add to my repository

--- js/app.js
+++ js/app.js
@@ -29,7 +29,9 @@ document.addEventListener('DOMContentLoaded',  function(){
 
 
 const date = new Date();
-date.setDate(1);
+
+const renderCalendar = () =>{
+    date.setDate(1);
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September","October", "November", "December"];
 const monthDays = document.querySelector(".days");
 const lastDay = new Date(date.getFullYear(), date.getMonth()+1, 0).getDate();
@@ -49,8 +51,12 @@ for(let x=firstDayIndex; x > 0; x--){
 }
 
 for (let i=1; i<=lastDay; i++){
-    if (i === new Date(),getDate)
-    days += `<div>${i}</div>`;
+    if (i === new Date(),getDate() && date.getMonth() === new Date().getMonth()){
+        days += `<div class="today">${i}</div>`
+    }
+    else{
+        days += `<div>${i}</div>`;
+    }
     
 }
 
@@ -59,4 +65,16 @@ for (let j=1; j<=nextDays;j++){
     monthDays.innerHTML = days;
 }
 
+}
 
+document.querySelector(".prev").addEventListener("click",() =>{
+    date.setMonth(date.getMonth() - 1);
+    renderCalendar();
+});
+
+document.querySelector(".next").addEventListener("click",() =>{
+    date.setMonth(date.getMonth() + 1);
+    renderCalendar();
+})
+
+renderCalendar();
