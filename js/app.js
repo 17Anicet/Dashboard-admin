64802033122ac93087502fb5a7f3512949f5d225
unknown <chizaanicet@gmail.com>
2023-06-12 20:58:02 +0200

    add to my repository

--- js/app.js
+++ js/app.js
@@ -29,14 +29,33 @@ document.addEventListener('DOMContentLoaded',  function(){
 
 
 const date = new Date();
+date.setDate(1);
 const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September","October", "November", "December"];
 const monthDays = document.querySelector(".days");
+const lastDay = new Date(date.getFullYear(), date.getMonth()+1, 0).getDate();
+const prevLastDay = new Date(date.getFullYear(), date.getMonth(), 0).getDate();
+const firstDayIndex = date.getDay();
+const lastDayIndex = new Date(date.getFullYear(), date.getMonth()+1, 0).getDay();
+const nextDays = 2 - lastDayIndex -1;
+
 
 document.querySelector(".date h4").innerHTML =  months[date.getMonth()];
 document.querySelector(".date p").innerHTML = date.toDateString();
 
 let days = "";
-for (let i=1; i<=30; i++ ){
+
+for(let x=firstDayIndex; x > 0; x--){
+    days += `<div class="prev-date">${prevLastDay - x + 1}</div>`;
+}
+
+for (let i=1; i<=lastDay; i++){
     days += `<div>${i}</div>`;
+    
+}
+
+for (let j=1; j<=nextDays;j++){
+    days += `<div class="next-date">${j}</div>`;
     monthDays.innerHTML = days;
 }
+
+
